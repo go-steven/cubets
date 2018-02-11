5d1af68276f6126ef4cbf42e2e13fd5a73aba5e8
?steven <m13581647166@163.com>
2018-02-11 17:36:59 +0800

    updated configs.

--- Config.ts
+++ Config.ts
@@ -1,9 +1,9 @@
 const Config: any = {
     db: {
-        host: "conn0845t2j7.mysql.rds.aliyuncs.com:3306",
-        user: "usr0845t2j7",
-        passwd: "d1gthsskd0345cricsuki",
-        dbname: "xibao_v2",
+        host     : 'xxx',
+        user     : 'xxx',
+        password : 'xxx',
+        database: 'xxx',
     }
 };
 
--- utils/dbconn.ts
+++ utils/dbconn.ts
@@ -2,10 +2,10 @@ import * as mysqldriver from 'mysql';
 
 var DefaultConn  = mysqldriver.createPool({
     connectionLimit : 10,
-    host     : 'conn0845t2j7.mysql.rds.aliyuncs.com',
-    user     : 'usr0845t2j7',
-    password : 'd1gthsskd0345cricsuki',
-    database: 'xibao_v2',
+    host     : 'xxx',
+    user     : 'xxx',
+    password : 'xxx',
+    database: 'xxx',
 });
 
 export {DefaultConn};
